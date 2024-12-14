f4bdf48caa885dec5b2780a6ee02f58f65134e78
SmokeyStack <seansyahril04@gmail.com>
2024-12-14 11:03:53 -0500

    Added Show More if contents are more than 10 entries

--- docs/.vitepress/theme/components/content/Table.vue
+++ docs/.vitepress/theme/components/content/Table.vue
@@ -13,7 +13,20 @@
         <tr v-for="(row, rowIndex) in tableData" :key="rowIndex">
           <td v-for="header in tableHeaders" :key="header">
             <ul v-if="Array.isArray(row[header])">
-              <li v-for="item in row[header]" :key="item">{{ item }}</li>
+              <li
+                v-for="(item, index) in row[header].slice(0, showLimit(rowIndex, header))"
+                :key="item"
+              >
+                {{ item }}
+              </li>
+
+              <span
+                v-if="row[header].length > showLimit(rowIndex, header)"
+                style="cursor: pointer; color: blue"
+                @click="expandList(rowIndex, header)"
+              >
+                Show more...
+              </span>
             </ul>
 
             <span v-else>{{ row[header] || "N/A" }}</span>
@@ -31,6 +44,8 @@ const tableData = ref([]);
 
 const tableHeaders = ref([]);
 
+const showLimits = ref({});
+
 const handleFileUpload = async (event) => {
   const file = event.target.files[0];
 
@@ -45,10 +60,32 @@ const handleFileUpload = async (event) => {
       tableHeaders.value = jsonData.header;
 
       tableData.value = jsonData.content;
+
+      initShowLimits();
     }
   };
 
   reader.readAsText(file);
+};
+
+const initShowLimits = () => {
+  tableData.value.forEach((row, rowIndex) => {
+    tableHeaders.value.forEach((header) => {
+      if (Array.isArray(row[header])) {
+        showLimits.value[`${rowIndex}-${header}`] = 10;
+      }
+    });
+  });
+};
+
+const showLimit = (rowIndex, header) => {
+  return showLimits.value[`${rowIndex}-${header}`] || 10;
+};
+
+const expandList = (rowIndex, header) => {
+  const key = `${rowIndex}-${header}`;
+
+  showLimits.value[key] = showLimits.value[key] + 10;
 };
 </script>
 
